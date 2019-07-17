6ea8aacb7916195516808955154e80c2dee43cb8
Asad-Chuadhry <52770102+Asad-Chuadhry@users.noreply.github.com>
2019-07-17 16:03:02 -0700

    Add files via upload

---
+++ Coding.cpp
@@ -0,0 +1,138 @@
+#include<iostream>
+#include<conio.h>
+#include<Windows.h>
+using namespace std;
+
+void heart()
+{
+	cout<< "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+		<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+		<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+		<< "\t\t\t\t\t\t =*              *=*              *=\n"
+		<< "\t\t\t\t\t\t =*                               *=\n"
+		<< "\t\t\t\t\t\t   =*         Programmer        *=\n"
+		<< "\t\t\t\t\t\t     =*          LOVE         *=\n"
+		<< "\t\t\t\t\t\t       =*                   *=\n"
+		<< "\t\t\t\t\t\t         =*               *=\n"
+		<< "\t\t\t\t\t\t           =*           *=\n"
+		<< "\t\t\t\t\t\t             =*       *=\n"
+		<< "\t\t\t\t\t\t               =*   *=\n"
+		<< "\t\t\t\t\t\t                 =*=\n";
+}
+int main()
+{
+	for (int i = 1;i<30;i++)
+	{
+		switch (i)
+		{
+		case 1: cout << "\n\n\n\n\n\t\t\t\t\t\t                 *=*                \n"; Sleep(1000); system("CLS"); break;
+		case 2: cout << "\n\n\n\n\t\t\t\t\t\t               *=   =*             \n"<< "\t\t\t\t\t\t                 *=*                \n"; Sleep(1000); system("CLS"); break;
+		case 3: cout << "\n\n\n\t\t\t\t\t\t             *=       =*         \n" << "\t\t\t\t\t\t               *=   =*             \n" << "\t\t\t\t\t\t                 *=*                \n"; Sleep(1000); system("CLS"); break;
+		case 4: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+					 << "\t\t\t\t\t\t             *=       =*         \n"
+					 << "\t\t\t\t\t\t               *=   =*             \n"
+			<< "\t\t\t\t\t\t                 *=*                \n"; Sleep(1000); system("CLS"); break;
+		case 5: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t               *=   =*             \n"
+			<< "\t\t\t\t\t\t                 *=*                \n"; Sleep(1000); system("CLS"); break;
+		case 6: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+			<< "\t\t\t\t\t\t                 *=*                \n"; Sleep(1000); system("CLS"); break;
+		case 7: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+			<< "\t\t\t\t\t\t =*              *=*              *=\n"; Sleep(1000); system("CLS"); break;
+		case 8: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+			<< "\t\t\t\t\t\t =*              *=*              *=\n"
+			<< "\t\t\t\t\t\t =*                               *=\n";  Sleep(1000); system("CLS"); break;
+		case 9: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+			<< "\t\t\t\t\t\t =*              *=*              *=\n"
+			<< "\t\t\t\t\t\t =*                               *=\n"
+			<< "\t\t\t\t\t\t   =*                           *=\n"; Sleep(1000); system("CLS"); break;
+		case 10: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+			<< "\t\t\t\t\t\t =*              *=*              *=\n"
+			<< "\t\t\t\t\t\t =*                               *=\n"
+			<< "\t\t\t\t\t\t   =*                           *=\n"
+			<< "\t\t\t\t\t\t     =*                       *=\n"; Sleep(1000); system("CLS"); break;
+		case 11: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+			<< "\t\t\t\t\t\t =*              *=*              *=\n"
+			<< "\t\t\t\t\t\t =*                               *=\n"
+			<< "\t\t\t\t\t\t   =*                           *=\n"
+			<< "\t\t\t\t\t\t     =*                       *=\n"
+			<< "\t\t\t\t\t\t       =*                   *=\n";  Sleep(1000); system("CLS"); break;
+		case 12: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+			<< "\t\t\t\t\t\t =*              *=*              *=\n"
+			<< "\t\t\t\t\t\t =*                               *=\n"
+			<< "\t\t\t\t\t\t   =*                           *=\n"
+			<< "\t\t\t\t\t\t     =*                       *=\n"
+			<< "\t\t\t\t\t\t       =*                   *=\n"
+			<< "\t\t\t\t\t\t         =*               *=\n"; Sleep(1000); system("CLS"); break;
+		case 13: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+			<< "\t\t\t\t\t\t =*              *=*              *=\n"
+			<< "\t\t\t\t\t\t =*                               *=\n"
+			<< "\t\t\t\t\t\t   =*                           *=\n"
+			<< "\t\t\t\t\t\t     =*                       *=\n"
+			<< "\t\t\t\t\t\t       =*                   *=\n"
+			<< "\t\t\t\t\t\t         =*               *=\n"
+			<< "\t\t\t\t\t\t           =*           *=\n"; Sleep(1000); system("CLS"); break;
+		case 14:cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+			<< "\t\t\t\t\t\t =*              *=*              *=\n"
+			<< "\t\t\t\t\t\t =*                               *=\n"
+			<< "\t\t\t\t\t\t   =*                           *=\n"
+			<< "\t\t\t\t\t\t     =*                       *=\n"
+			<< "\t\t\t\t\t\t       =*                   *=\n"
+			<< "\t\t\t\t\t\t         =*               *=\n"
+			<< "\t\t\t\t\t\t           =*           *=\n"
+			<< "\t\t\t\t\t\t             =*       *=\n"; Sleep(1000); system("CLS"); break;
+		case 15: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+			<< "\t\t\t\t\t\t =*              *=*              *=\n"
+			<< "\t\t\t\t\t\t =*                               *=\n"
+			<< "\t\t\t\t\t\t   =*                           *=\n"
+			<< "\t\t\t\t\t\t     =*                       *=\n"
+			<< "\t\t\t\t\t\t       =*                   *=\n"
+			<< "\t\t\t\t\t\t         =*               *=\n"
+			<< "\t\t\t\t\t\t           =*           *=\n"
+			<< "\t\t\t\t\t\t             =*       *=\n"
+			<< "\t\t\t\t\t\t               =*   *=\n"; Sleep(1000); system("CLS"); break;
+		case 16: cout << "\n\n\t\t\t\t\t\t      =*****=           =*****=\n"
+			<< "\t\t\t\t\t\t    =*       *=       =*       *=\n"
+			<< "\t\t\t\t\t\t  =*           *=   =*           *=\n"
+			<< "\t\t\t\t\t\t =*              *=*              *=\n"
+			<< "\t\t\t\t\t\t =*                               *=\n"
+			<< "\t\t\t\t\t\t   =*                           *=\n"
+			<< "\t\t\t\t\t\t     =*                       *=\n"
+			<< "\t\t\t\t\t\t       =*                   *=\n"
+			<< "\t\t\t\t\t\t         =*               *=\n"
+			<< "\t\t\t\t\t\t           =*           *=\n"
+			<< "\t\t\t\t\t\t             =*       *=\n"
+			<< "\t\t\t\t\t\t               =*   *=\n"
+			<< "\t\t\t\t\t\t                 =*=\n";  Sleep(1000); system("CLS"); break;
+		
+		default: SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), i-15);
+			heart();
+			Sleep(1000); system("CLS");  break;
+}
+	 }
+	 heart();
+	 cout << "\n\t\t\t\t\t\t         Thanks For Watching..";
+		
+	return 0;
+}
